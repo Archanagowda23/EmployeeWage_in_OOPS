dd2a9303b86106820d45bb87cab27ff314bdbd2b
Archanagowda23 <archanar249@gmail.com>
2022-03-11 21:20:05 +0530

    Added UC3:to check employee part time wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -32,11 +32,32 @@ public class EmployeeWage {
 
     }
 
+    public void PartTimeEmpWage(){
+        int EmpWagePerHr = 20;
+        int EmpHr = 8;
+        int IsFullTime = 1;
+        int IsPartTime = 2;
+        int Salary = 0;
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IsFullTime) {
+            Salary = (EmpHr * EmpWagePerHr);
+            System.out.println("Employee salary for full time is: " + Salary);
+        } else if (empCheck == IsPartTime) {
+            EmpHr = 4;
+            Salary = (EmpHr * EmpWagePerHr);
+            System.out.println("Employee salary for part time is: " + Salary);
+        } else
+            System.out.println("Employee salary for absent is :" + Salary);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to employee wage");
         EmployeeWage object = new EmployeeWage();
 
         object.EmployeeAttendance();
         object.CalculateDailyWage();
+        object.PartTimeEmpWage();
     }
 }
