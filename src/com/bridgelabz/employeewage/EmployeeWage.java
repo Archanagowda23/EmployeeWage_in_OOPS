2a2eee8c30cb89ff6ab1293387830e0521dcd372
Archanagowda23 <archanar249@gmail.com>
2022-03-11 21:14:13 +0530

    Added UC2: to calculate empwage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Scanner;
+
 public class EmployeeWage {
 
     public void EmployeeAttendance(){
@@ -12,10 +14,29 @@ public class EmployeeWage {
 
     }
 
+    public void CalculateDailyWage(){
+        System.out.println("Enter numbers of hours worked");
+        Scanner scanner = new Scanner(System.in);
+        double totalHoursWorked = scanner.nextInt();
+        double standardWages = 20;
+        int standardHours = 8;
+        double totalWage;
+        if (totalHoursWorked > 8) {
+            totalWage = (standardHours * totalHoursWorked) + (totalHoursWorked - standardHours);
+        } else if (totalHoursWorked < 8) {
+            totalWage = standardWages * totalHoursWorked;
+        } else {
+            totalWage = standardWages * standardHours;
+        }
+        System.out.println("MY total wages of full day is:" + totalWage);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome to employee wage");
         EmployeeWage object = new EmployeeWage();
 
         object.EmployeeAttendance();
+        object.CalculateDailyWage();
     }
 }
